4b4258d6ebc11088bdf934651d4e55fdd062b06a
Jonathan Xina <john.xina.p88@gmail.com>
2025-01-28 15:30:03 +0700

    feat: can call to backend

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -85,7 +85,6 @@ import { confluxPlugin } from "@elizaos/plugin-conflux";
 import { createCosmosPlugin } from "@elizaos/plugin-cosmos";
 import { cronosZkEVMPlugin } from "@elizaos/plugin-cronoszkevm";
 import { deskExchangePlugin } from "@elizaos/plugin-desk-exchange";
-import { deskExchangePlugin } from "@elizaos/plugin-desk-exchange";
 import { evmPlugin } from "@elizaos/plugin-evm";
 import { edwinPlugin } from "@elizaos/plugin-edwin";
 import { flowPlugin } from "@elizaos/plugin-flow";
@@ -1524,9 +1523,7 @@ const startAgents = async () => {
 
     directClient.start(serverPort);
 
-    if (serverPort !== Number.parseInt(settings.SERVER_PORT || "3000")) {
-        elizaLogger.log(`Server started on alternate port ${serverPort}`);
-    }
+    elizaLogger.info(`Server started on alternate port ${serverPort}`);
 
     elizaLogger.info(
         "Run `pnpm start:client` to start the client and visit the outputted URL (http://localhost:5173) to chat with your agents. When running multiple agents, use client with different port `SERVER_PORT=3001 pnpm start:client`"
