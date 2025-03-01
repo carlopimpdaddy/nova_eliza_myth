8c807c66b756d0f3d4b5f09663b72da729013e6f
TULPtechnologies <carlo.dekker@tulp.app>
2025-03-01 19:30:50 +0100

    new

--- eliza-start.sh
+++ eliza-start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd /app/eliza/agent
 export NODE_OPTIONS="--no-warnings"
-npx ts-node src/index.ts --isRoot --plugin twitter --autopost --debug 
+# Use a more explicit ts-node command with proper flags for ESM support
+npx ts-node --esm --transpile-only --skip-project src/index.ts --isRoot --plugin twitter --autopost --debug 
